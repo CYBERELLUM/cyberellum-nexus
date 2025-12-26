a7957448dfc4d34fb3696e144ddf2eeb470b4401
Cyberellum Technologies & Laboratory <laz@goodfellas.agency>
2025-12-26 13:57:51 -0500

    [FEDERATION] Add countdown timer to Coming Soon banner

--- src/components/BetaNotice.tsx
+++ src/components/BetaNotice.tsx
@@ -1,4 +1,11 @@
-// Cyberellum Nexus - Production Website
-// Beta notice disabled for main website
-export const BetaNotice = () => null;
+/**
+ * BETA NOTICE - Includes Coming Soon Banner
+ * Cyberellum Federation Satellite
+ * Last Updated: 2025-12-26
+ */
+
+import ComingSoonBanner from './ComingSoonBanner';
+
+export const BetaNotice = () => <ComingSoonBanner />;
+
 export default BetaNotice;
